c055aeae0ec34d995b3b636fb1c785f0ca35bf42
Felix Gnass <fgnass@gmail.com>
2014-02-03 22:09:56 +0100

    support exposing streams

--- test/index.js
+++ test/index.js
@@ -1,22 +1,45 @@
 var exposed = require('..')
   , express = require('express')
   , request = require('supertest')
+  , concat = require('concat-stream')
   , should = require('should')
-
+  , fs = require('fs')
 
 var resources = exposed()
-resources.expose({ path: '/foo.txt', content: 'Foo' })
-resources.expose({ path: '/hello.txt', root: __dirname })
-resources.expose({ file: __dirname + '/foo.css', root: __dirname })
-
-
-var zeros = new Array(1024).join(0)
-resources.expose({ path: '/zip', content: zeros, gzip: true })
 
 var app = express()
 app.use('/mounted', resources)
 
+resources.expose({ path: '/foo.txt', content: 'Foo' })
+resources.expose({ path: '/hello.txt', root: __dirname })
+resources.expose({ file: __dirname + '/foo.css', root: __dirname })
+
+var zeros = new Array(1024).join(0)
+resources.expose({ path: '/zip', content: zeros, gzip: true })
+
 describe('expose string', function() {
+
+  it('should iterate over all resources', function() {
+    var all = []
+    resources.each(function(res) {
+      all.push(res)
+    })
+    all.length.should.equal(4)
+    all[0].path.should.equal('/foo.txt')
+    all[1].path.should.equal('/hello.txt')
+  })
+
+  it('should lookup resources', function(done) {
+    resources.get('/hello.txt').createReadStream().pipe(concat(function(s) {
+      s.toString().should.equal('hello')
+      done()
+    }))
+  })
+
+  it('should know the mount point', function() {
+    resources.get('/hello.txt').url.should.equal('/mounted/hello.txt')
+  })
+
   it('should expose strings', function(done) {
     request(app)
       .get('/mounted/foo.txt')
@@ -24,10 +47,12 @@ describe('expose string', function() {
       .end(done)
   })
 
-  it('should report modifications', function() {
-    resources.expose({ path: '/foo', content: 'foo' }).dirty.should.be.ok
-    resources.expose({ path: '/foo', content: 'foo' }).dirty.should.not.be.ok
-    resources.expose({ path: '/foo', content: 'bar' }).dirty.should.be.ok
+  it('should report modifications', function(done) {
+    resources.on('change', function(res) {
+      done()
+    })
+    resources.expose({ path: '/foo', content: 'foo' })
+    resources.expose({ path: '/foo', content: 'bar' })
   })
 
   it('should expose files', function(done) {
@@ -38,18 +63,13 @@ describe('expose string', function() {
       .end(done)
   })
 
-  it('should lookup resources', function() {
-    String(resources.get('/hello.txt')).should.equal('hello')
-  })
-
-  it('should iterate over all resources', function() {
-    var all = []
-    resources.each(function(res) {
-      all.push(res)
-    })
-    all.length.should.equal(5)
-    all[0].path.should.equal('/foo.txt')
-    all[0].toString().should.equal('Foo')
+  it('should expose streams', function(done) {
+    var f = __dirname + '/hello.txt'
+    resources.expose({path: '/stream', from: fs.createReadStream(f)})
+    request(app)
+      .get('/mounted/stream')
+      .expect(200, 'hello')
+      .end(done)
   })
 
   it('should determine path based on root', function(done) {
